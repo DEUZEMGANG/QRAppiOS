61d934bc37adf70f8098a929040c0bf879d15e2d
merih <merihhatay@gmail.com>
2023-11-23 15:02:06 +0300

    first commit

---
+++ QRApp/QRApp/QRApp/QRAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRAppApp.swift
+//  QRApp
+//
+//  Created by merih on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct QRAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
